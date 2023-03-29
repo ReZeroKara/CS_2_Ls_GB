2c91d1342dc95f52fe0f855e6fcb6e4cae461d76
ReZero <ghostofpast@icloud.com>
2023-03-30 00:15:45 +0300

    add ArrayMethod pj

---
+++ project004_ArrayMethod/Program.cs
@@ -0,0 +1,26 @@
+﻿void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+void PrintArray(int[] shelf)
+{
+    int closet = shelf.Length;
+    int position = 0;
+    while (position < closet)
+    {
+        Console.WriteLine(shelf[position]);
+        position++;
+    }
+}
+
+int[] array = new int[10];
+
+FillArray(array);
+PrintArray(array);
